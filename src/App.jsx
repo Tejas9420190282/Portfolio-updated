2f281c07f9702315b90d3ce2d738d9fa3173a343
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-09-17 19:48:45 +0530

    New project added

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Turf_Finder_Project_Details from './pages/Turf_Finder_Project_Details'
 import Chatting_App_Using_Firebase_Project_Details from './pages/Chatting_App_Using_Firebase_Project_Details'
 import Airline_Reservation_System_Project_Details from './pages/Airline_Reservation_System_Project_Details'
+import Doctor_Appointment_System_Project_Details from './pages/Doctor_Appointment_System_Project_Details'
 
 function App() {
 
@@ -30,6 +31,12 @@ function App() {
       path : "/project-airline-reservation-system-details",
       element : <><Airline_Reservation_System_Project_Details /></>
     },
+    {
+      path : "/project-doctor-appointment-system-details",
+      element : <><Doctor_Appointment_System_Project_Details /></>
+    },
+
+    
 
   ])
 
--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import ABC from "../assets/ABC.png";
 import turf_Finder_img from "../assets/turf_Finder_img.png";
 import chat_app_img from "../assets/chat_app_img.png";
+import doctor from "../assets/doctor.png";
+
 import { useNavigate } from "react-router-dom";
 
 function Portfolio() {
@@ -21,6 +23,10 @@ function Portfolio() {
       navigate("/project-airline-reservation-system-details")
     }
 
+    const handle_Doctors_Appointment_System = () => {
+      navigate("/project-doctor-appointment-system-details")
+    }
+
     return (
         <>
             <div id="portfolio">
@@ -30,8 +36,9 @@ function Portfolio() {
                 <p className="text-[#49A7EC] text-3xl mt-2 font-bold flex justify-center">
                     Portfolio
                 </p>
-                <div className="md:flex mt-10 justify-center items-center md:justify-center md:items-center ">
-                    <div className="bg-[#2C2C6C] md:mr-5 h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
+                <div className="md:flex mt-10 justify-center items-center md:justify-center md:items-center flex-wrap gap-10">
+                    {/* Project 1 */}
+                    <div className="bg-[#2C2C6C]  h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
                                 src={turf_Finder_img}
@@ -47,8 +54,8 @@ function Portfolio() {
                             <button className="bg-[#49A7EC] text-black font-semibold hover:bg-white" onClick={handle_Turf_Finder_view_details}>View Details</button>
                         </div>
                     </div>
-
-                    <div className="bg-[#2C2C6C] md:ml-5 h-96 w-80 md:w-[390px] md:h-[461px] mt-14 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
+                    {/* Project 2 */}
+                    <div className="bg-[#2C2C6C]  h-96 w-80 md:w-[390px] md:h-[461px] mt-14 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
                                 src={chat_app_img}
@@ -67,10 +74,13 @@ function Portfolio() {
                             <button className="bg-[#49A7EC] text-black font-semibold hover:bg-white" onClick={handle_Chatting_App_Using_Firebase}>View Details</button>
                         </div>
                     </div>
+
+                    
                 </div>
 
-                {/*   Project3  */}
-                <div className="md:flex justify-center md:mt-10">
+
+                <div className="md:flex justify-center md:mt-10 gap-10">
+                   {/* {/*   Project 3  */}
                     <div className="bg-[#2C2C6C] h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
@@ -82,16 +92,27 @@ function Portfolio() {
                             Airline Reservation system
                         </p>
                         <div className="flex justify-center gap-5 mt-5">
-                            {/* <button className="bg-[#2C2C6C] text-[#49A7EC] border-[#49A7EC] font-semibold hover:bg-white hover:text-black hover:border-none" onClick={gitAirlineReservationSystem}>
-                                github
-                            </button>
-                            <button className="bg-[#49A7EC] text-black font-semibold hover:bg-white">
-                                Live Demo
-                            </button> */}
 
                             <button className="bg-[#49A7EC] text-black font-semibold hover:bg-white" onClick={handle_Airline_Reservation_System}>View Details</button>
                         </div>
                     </div>
+
+                    {/*   Project 4 */}
+                    <div className="bg-[#2C2C6C] h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
+                        <div className="flex justify-center items-center">
+                            <img
+                                src={doctor}
+                                className="h-[200px] w-[250px] mt-7 md:h-[280px] md:w-[320px] rounded-2xl"
+                            />
+                        </div>
+                        <p className="mt-8 text-left ml-8  text-lg lg:text-xl font-bold ">
+                            Doctor's Appointment system
+                        </p>
+                        <div className="flex justify-center gap-5 mt-5">
+
+                            <button className="bg-[#49A7EC] text-black font-semibold hover:bg-white text-center" onClick={handle_Doctors_Appointment_System}>View Details</button>
+                        </div>
+                    </div> 
                 </div>
             </div>
         </>
