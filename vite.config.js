ada54e403f9d18f24b3ac65195a892130cc047d5
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-10-22 10:50:50 +0530

    updates in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  vite: "/Portfolio-updated/",
+  plugins: [
+    react({
+      fastRefresh: false, // 🚫 disable fast refresh
+    }),
+  ],
+  base: '/',
 })
