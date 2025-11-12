ca800ec01d72ee6cc90123f24a0670d18e1130c4
“Tejas9420190282” <tejasshimpi877@gmail.com>
2025-11-12 19:53:11 +0530

    update files 'Contact.jsx' and 'Portfollio.jsx'

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,3 +1,4 @@
+/* 
 import React, { useState } from "react";
 
 import { MdOutlineMail } from "react-icons/md";
@@ -153,4 +154,174 @@ function Contact() {
 }
 
 export default Contact;
- 
+  */
+
+
+import React, { useState } from "react";
+import { MdOutlineMail } from "react-icons/md";
+import { FaLinkedin, FaWhatsapp } from "react-icons/fa";
+import cv from "../assets/Tejas_Shimpi_CV.pdf";
+
+function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleEmailClick = () => {
+    window.location.href = "mailto:tejasshimpi87@gmail.com";
+  };
+
+  const handleLinkedinClick = () => {
+    const linkedInUrl =
+      "https://www.linkedin.com/in/tejas-shimpi-459235206?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+    window.open(linkedInUrl, "_blank");
+  };
+
+  const handleWhatsAppClick = () => {
+    const phoneNumber = "9420190282";
+    const whatsappUrl = `https://wa.me/${phoneNumber}`;
+    window.open(whatsappUrl, "_blank");
+  };
+
+  const handleSubmit = () => {
+    const whatsappNumber = "9420190282";
+    const text = `Name : ${name} \n Email : ${email} \n Message : ${message}`;
+    const url = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(
+      text
+    )}`;
+
+    window.open(url, "_blank");
+    alert("Thank you for contacting me!");
+  };
+
+  return (
+    <div
+      id="contact"
+      className="px-4 sm:px-6 md:px-10 lg:px-20 flex flex-col items-center"
+    >
+      {/* Header */}
+      <p className="text-[#9D9DA6] font-semibold mt-28 text-md text-center">
+        Get In Touch
+      </p>
+      <p className="text-[#49A7EC] text-3xl mt-2 font-bold text-center">
+        Contact Me
+      </p>
+
+      {/* Flex container */}
+      <div className="flex flex-col md:flex-row justify-center items-center md:items-start md:gap-16 mt-10 w-full max-w-6xl">
+        {/* Left Section */}
+        <div className="flex flex-col items-center md:items-start gap-6 w-full md:w-1/2 lg:w-2/5">
+          {/* Email Card */}
+          <div className="w-full max-w-xs sm:max-w-sm md:w-full h-40 md:h-32 rounded-2xl bg-[#2C2C6C] hover:bg-[#1b1b31] hover:border-2 hover:border-[#49A7EC] transition">
+            <div className="pt-5 md:pt-3 flex justify-center">
+              <MdOutlineMail className="text-3xl text-white" />
+            </div>
+            <p className="mt-1 text-2xl font-bold md:text-base md:mt-1 text-white text-center">
+              Email
+            </p>
+            <p className="font-semibold text-xl mt-1 md:text-sm md:mt-1 text-white text-center">
+              tejasshimpi877@gmail.com
+            </p>
+            <p
+              className="mt-2 text-[#49A7EC] text-lg cursor-pointer md:mt-0 text-center"
+              onClick={handleEmailClick}
+            >
+              send a message
+            </p>
+          </div>
+
+          {/* LinkedIn Card */}
+          <div className="w-full max-w-xs sm:max-w-sm md:w-full h-44 md:h-32 rounded-2xl bg-[#2C2C6C] hover:bg-[#1b1b31] hover:border-2 hover:border-[#49A7EC] transition">
+            <div className="pt-5 md:pt-3 flex justify-center">
+              <FaLinkedin className="text-3xl text-white" />
+            </div>
+            <p className="mt-2 text-2xl font-bold md:text-base md:mt-1 text-white text-center">
+              LinkedIn
+            </p>
+            <p className="font-semibold text-xl mt-2 md:text-sm md:mt-1 text-white text-center">
+              Tejas Shimpi
+            </p>
+            <p
+              className="mt-2 text-[#49A7EC] text-lg cursor-pointer md:mt-0 text-center"
+              onClick={handleLinkedinClick}
+            >
+              send a message
+            </p>
+          </div>
+
+          {/* WhatsApp Card */}
+          <div className="w-full max-w-xs sm:max-w-sm md:w-full h-44 md:h-32 rounded-2xl bg-[#2C2C6C] hover:bg-[#1b1b31] hover:border-2 hover:border-[#49A7EC] transition">
+            <div className="pt-5 md:pt-3 flex justify-center">
+              <FaWhatsapp className="text-3xl text-white" />
+            </div>
+            <p className="mt-2 text-2xl font-bold md:text-base md:mt-1 text-white text-center">
+              WhatsApp
+            </p>
+            <p className="font-semibold text-xl mt-2 md:text-sm md:mt-1 text-white text-center">
+              +91 9420190282
+            </p>
+            <p
+              className="mt-2 text-[#49A7EC] text-lg cursor-pointer md:mt-0 text-center"
+              onClick={handleWhatsAppClick}
+            >
+              send a message
+            </p>
+          </div>
+        </div>
+
+        {/* Right Section */}
+        <div className="flex flex-col items-center md:items-start mt-10 md:mt-0 w-full md:w-1/2 lg:w-3/5">
+          {/* Name Input */}
+          <div className="w-full max-w-sm md:max-w-lg bg-[#242424] flex justify-center items-center rounded-xl border-2 border-[#49A7EC] h-14 md:h-14">
+            <input
+              type="text"
+              value={name}
+              placeholder="Your Full Name"
+              onChange={(e) => setName(e.target.value)}
+              className="w-full bg-[#242424] outline-none px-4 text-white"
+            />
+          </div>
+
+          {/* Email Input */}
+          <div className="w-full max-w-sm md:max-w-lg bg-[#242424] flex justify-center items-center rounded-xl border-2 border-[#49A7EC] h-14 mt-5">
+            <input
+              type="email"
+              value={email}
+              placeholder="Your Email"
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full bg-[#242424] outline-none px-4 text-white"
+            />
+          </div>
+
+          {/* Message Box */}
+          <div className="w-full max-w-sm md:max-w-lg bg-[#242424] flex justify-center items-center rounded-xl border-2 border-[#49A7EC] mt-5">
+            <textarea
+              rows="5"
+              value={message}
+              placeholder="Enter message"
+              onChange={(e) => setMessage(e.target.value)}
+              className="resize-none outline-none w-full bg-[#242424] text-white px-4 py-3"
+            ></textarea>
+          </div>
+
+          {/* Buttons */}
+          <div className="flex justify-center items-center mt-8 gap-3 flex-wrap">
+            <button
+              className="h-12 w-36 flex justify-center items-center text-black font-semibold rounded-lg bg-[#49A7EC] cursor-pointer hover:bg-white transition"
+              onClick={handleSubmit}
+            >
+              Send Message
+            </button>
+            <a href={cv} download>
+              <button className="h-12 w-36 bg-[#1B1B31] text-[#49A7EC] font-semibold rounded-lg border border-[#49A7EC] hover:bg-white hover:text-black transition">
+                Download CV
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,4 +1,4 @@
-
+/* 
 import React from "react";
 import ABC from "../assets/ABC.png";
 import turf_Finder_img from "../assets/turf_Finder_img.png";
@@ -38,7 +38,7 @@ function Portfolio() {
                     Portfolio
                 </p>
                 <div className="md:flex mt-10 justify-center items-center md:justify-center md:items-center flex-wrap gap-10">
-                    {/* Project 1 */}
+                    {/* Project 1 
                     <div className="bg-[#2C2C6C]  h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
@@ -55,7 +55,7 @@ function Portfolio() {
                             <button className="bg-[#49A7EC] text-black font-semibold hover:bg-white" onClick={handle_Turf_Finder_view_details}>View Details</button>
                         </div>
                     </div>
-                    {/* Project 2 */}
+                    {/* Project 2 
                     <div className="bg-[#2C2C6C]  h-96 w-80 md:w-[390px] md:h-[461px] mt-14 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
@@ -81,7 +81,7 @@ function Portfolio() {
 
 
                 <div className="md:flex justify-center md:mt-10 gap-10">
-                   {/* {/*   Project 3  */}
+                   {/* {/*   Project 3  
                     <div className="bg-[#2C2C6C] h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
@@ -98,7 +98,7 @@ function Portfolio() {
                         </div>
                     </div>
 
-                    {/*   Project 4 */}
+                    {/*   Project 4 
                     <div className="bg-[#2C2C6C] h-96 w-80 md:w-[390px] md:h-[461px] mt-10 md:mt-0 rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC]">
                         <div className="flex justify-center items-center">
                             <img
@@ -118,6 +118,136 @@ function Portfolio() {
             </div>
         </>
     );
+}
+
+export default Portfolio;
+ */
+
+
+
+import React from "react";
+import ABC from "../assets/ABC.png";
+import turf_Finder_img from "../assets/turf_Finder_img.png";
+import chat_app_img from "../assets/chat_app_img.png";
+import doctor from "../assets/doctor.png";
+import airline from "../assets/Airline.png";
+
+import { useNavigate } from "react-router-dom";
+
+function Portfolio() {
+  const navigate = useNavigate();
+
+  const handle_Turf_Finder_view_details = () => {
+    navigate("/project-turf-finder-details");
+  };
+
+  const handle_Chatting_App_Using_Firebase = () => {
+    navigate("/project-chatting-app-using-firebase-details");
+  };
+
+  const handle_Airline_Reservation_System = () => {
+    navigate("/project-airline-reservation-system-details");
+  };
+
+  const handle_Doctors_Appointment_System = () => {
+    navigate("/project-doctor-appointment-system-details");
+  };
+
+  return (
+    <>
+      <div id="portfolio" className="px-4 md:px-10 lg:px-20">
+        <p className="text-[#9D9DA6] font-semibold mt-28 text-md text-center">
+          My Recent Work
+        </p>
+        <p className="text-[#49A7EC] text-3xl mt-2 font-bold text-center">
+          Portfolio
+        </p>
+
+        {/* Project Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center mt-10">
+          {/* Project 1 */}
+          <div className="bg-[#2C2C6C] w-full max-w-[390px] rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC] p-5 flex flex-col items-center transition-all duration-300">
+            <img
+              src={turf_Finder_img}
+              alt="Turf Finder"
+              className="w-full h-auto rounded-2xl object-cover"
+            />
+            <p className="mt-6 text-left w-full text-2xl font-bold">
+              Turf Finder
+            </p>
+            <div className="flex justify-center gap-5 mt-5">
+              <button
+                className="bg-[#49A7EC] text-black font-semibold hover:bg-white px-4 py-2 rounded-md"
+                onClick={handle_Turf_Finder_view_details}
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+
+          {/* Project 2 */}
+          <div className="bg-[#2C2C6C] w-full max-w-[390px] rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC] p-5 flex flex-col items-center transition-all duration-300">
+            <img
+              src={chat_app_img}
+              alt="Chat App"
+              className="w-full h-auto rounded-2xl object-cover"
+            />
+            <p className="mt-6 text-left w-full text-xl font-bold">
+              Chatting App using Firebase
+            </p>
+            <div className="flex justify-center gap-5 mt-5">
+              <button
+                className="bg-[#49A7EC] text-black font-semibold hover:bg-white px-4 py-2 rounded-md"
+                onClick={handle_Chatting_App_Using_Firebase}
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+
+          {/* Project 3 */}
+          <div className="bg-[#2C2C6C] w-full max-w-[390px] rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC] p-5 flex flex-col items-center transition-all duration-300">
+            <img
+              src={airline}
+              alt="Airline Reservation System"
+              className="w-full h-auto rounded-2xl object-cover"
+            />
+            <p className="mt-6 text-left w-full text-xl font-bold">
+              Airline Reservation System
+            </p>
+            <div className="flex justify-center gap-5 mt-5">
+              <button
+                className="bg-[#49A7EC] text-black font-semibold hover:bg-white px-4 py-2 rounded-md"
+                onClick={handle_Airline_Reservation_System}
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+
+          {/* Project 4 */}
+          <div className="bg-[#2C2C6C] w-full max-w-[390px] rounded-3xl hover:bg-[#1F1F38] hover:border-2 hover:border-[#49A7EC] p-5 flex flex-col items-center transition-all duration-300">
+            <img
+              src={doctor}
+              alt="Doctor Appointment System"
+              className="w-full h-auto rounded-2xl object-cover"
+            />
+            <p className="mt-6 text-left w-full text-lg lg:text-xl font-bold">
+              Doctor's Appointment System
+            </p>
+            <div className="flex justify-center gap-5 mt-5">
+              <button
+                className="bg-[#49A7EC] text-black font-semibold hover:bg-white px-4 py-2 rounded-md"
+                onClick={handle_Doctors_Appointment_System}
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Portfolio;
